3921258dfce13ef16a79d70469af08eb27dc7d87
Victor Borja <vborja@apache.org>
2013-01-11 23:04:03 -0600

    make symlinked freshrc

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -e
 # vim: set ts=2 sw=2:
 
-if [ ! -f ~/.fresh/source/freshshell/fresh/bin/fresh ]; then
+FRESH_REPO=vic/...
+FRESH_RC=config/freshrc
+
+if [ ! -f ~/.freshrc ]; then
   curl -sL get.freshshell.com | bash
-  source ~/.fresh/build.new/shell.sh
-  fresh vic/... config/freshrc --file
+  source ~/.fresh/build/shell.sh
+  echo "fresh $FRESH_REPO $FRESH_RC --file=~/.fresh/start" >> ~/.freshrc
+  fresh install --quiet
+  ln -sf ~/.fresh/source/$FRESH_REPO/$FRESH_RC ~/.freshrc
 fi
 
 fresh
