e00ada4494ba64e355cc974a75ee1ef549d47787
Victor Borja <vborja@apache.org>
2013-01-11 23:36:18 -0600

    fresh only once

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -e
 # vim: set ts=2 sw=2:
 
-FRESH_REPO=vic/...
-FRESH_RC=config/freshrc
+FRESH_REPO="${1:-vic/...}"
+FRESH_RC="${2:-config/freshrc}"
+
+cd
 
 if [ ! -f ~/.freshrc ]; then
   curl -sL get.freshshell.com | bash
   source ~/.fresh/build/shell.sh
-  echo "fresh $FRESH_REPO $FRESH_RC --file=~/.fresh/start" >> ~/.freshrc
-  fresh install --quiet
+  git clone git://github.com/$FRESH_REPO.git ~/.fresh/source/$FRESH_REPO
   ln -sf ~/.fresh/source/$FRESH_REPO/$FRESH_RC ~/.freshrc
 fi
 
