d6ad33d80b8b1ccc3b0a2bcfd227289f6d7dce40
Victor Borja <vborja@apache.org>
2013-01-11 22:00:14 -0600

    Install fresh

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -e
 # vim: set ts=2 sw=2:
 
-TREEDOT=vic/...
-
 if [ ! -f ~/.fresh/source/freshshell/fresh/bin/fresh ]; then
-  echo > ~/.freshrc 
   curl -sL get.freshshell.com | bash
-  curl -sL https://raw.github.com/$TREEDOT/master/startfresh  >> ~/.freshrc
+  source ~/.fresh/build.new/shell.sh
+  fresh vic/... config/freshrc --file
 fi
 
-source ~/.fresh/build/shell.sh
 fresh
