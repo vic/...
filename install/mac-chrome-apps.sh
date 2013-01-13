6da673a0b450d848adbeed3f8410857f425d2266
Victor Borja <vborja@apache.org>
2013-01-12 18:34:40 -0600

    Only create chrome apps that are missing

--- install/mac-chrome-apps.sh
+++ install/mac-chrome-apps.sh
@@ -1,7 +1,12 @@
 
 # The makeapp script can be found on script/
-MAKEAPP=${1:-sh $FRESH_MINE/script/makeapp.sh}
+MAKEAPP=${MAKEAPP:-$FRESH_MINE/script/makeapp.sh}
 
-$MAKEAPP Google+ http://plus.google.com http://plus.google.com/favicon.ico
-$MAKEAPP Gmail http://mail.google.com http://mail.google.com/favicon.ico
-$MAKEAPP GitHub http://github.com http://octodex.github.com/images/octocat-de-los-muertos.jpg  # http://octodex.github.com/images/baracktocat.jpg 
+[ ! -d ~/Applications/Google+.app/ ] &&
+    sh $MAKEAPP Google+ http://plus.google.com http://plus.google.com/favicon.ico
+
+[ ! -d ~/Applications/Gmail.app/ ] &&
+sh $MAKEAPP Gmail http://mail.google.com http://mail.google.com/favicon.ico
+
+[ ! -d ~/Applications/GitHub.app/ ] &&
+sh $MAKEAPP GitHub http://github.com http://octodex.github.com/images/baracktocat.jpg 
